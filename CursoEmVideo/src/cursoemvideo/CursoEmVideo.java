c122a02aef96e72bf52817cb7c8397dbaf636877
CaioCorchado <kaitohiraga2015@gmail.com>
2018-01-03 02:00:09 -0200

    Curso Em Video
    Aplicação básica (simples), Swing e JavaFX
    
    03/01/2018 - 02:00

---
+++ CursoEmVideo/src/cursoemvideo/CursoEmVideo.java
@@ -0,0 +1,13 @@
+
+package cursoemvideo;
+
+public class CursoEmVideo {
+    
+//psvm + tab
+    public static void main(String[] args) {
+          //sout + tab
+        System.out.println("Caio Diogenes Venancio Corchado - 3ºB2");
+        System.out.println("03/01/18 01:03");
+    }
+    
+}
